d08b4173d52c03e078502387d684d0e8b761a094
makaka <75128685+makk521@users.noreply.github.com>
2023-09-19 22:19:50 +0800

    添加c++远程存入Redis数据库功能
    具体细节在code_repository；
    实现c++远程读存Redis数据库

--- ProjectDevelop/Progress/v1/ClientPost.py
+++ ProjectDevelop/Progress/v1/ClientPost.py
@@ -8,7 +8,7 @@ class Student(BaseModel):
     is_man: Optional[bool] = None
 
 if __name__ == "__main__":
-    postData = {"name": "lu", "age": 3, "is_man": False}
+    postData = {"name": "lu", "age": 9, "is_man": False}
     # response = requests.get('http://124.223.76.58:5000/foo/makk?age=12')
     response = requests.post('http://124.223.76.58:8000/foo/makk', json=postData)
     print(response.content)
---
+++ ProjectStudy/redisRemote/readme.md
@@ -0,0 +1,12 @@
+#### 目标
+
+c++实现Redis的远程读写和存储。
+
+#### 操作
+
+将redisconnect内所有文件copy到云服务器中(``ustc_private-/home/ustc-lc1/mq/Dji/redisconnect``)，运行redisTest.cpp操作另一台服务器(腾讯)。
+
+
+#### 参考
+
+[code_repository](https://github.com/makk521/code-repository/blob/main/makaka/Redis%E5%9F%BA%E7%A1%80/readme.md)
