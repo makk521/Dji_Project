e66c646e62c0a3787013d9d1f5a0181ce07e08ed
makaka <75128685+makk521@users.noreply.github.com>
2023-10-18 12:28:20 +0800

    增加结构体转Json的示例
    1:TimerFunction.cpp ----> 服务器运行实现每1秒刷新数据库，数据为各结构体转成一个Json，即最终形式
    2:RedisStruTest.cpp ----->本地使用模板函数实现结构体转Json

--- ProjectStudy/StudyC++/Basic/RedisStruTest.cpp
+++ ProjectStudy/StudyC++/Basic/RedisStruTest.cpp
@@ -1,18 +1,10 @@
+/*
+本地运行，模板函数实现结构体转Json，实际使用重载函数
+*/
 #include <iostream>
 #include <nlohmann/json.hpp>
 
 using json = nlohmann::json;
-
-template <typename T>
-json structToJson(const T& obj) {
-    json jsonObj;
-
-    jsonObj["x"] = obj.x;
-    jsonObj["y"] = obj.y;
-    jsonObj["z"] = obj.z;
-
-    return jsonObj;
-}
 
 struct Vector3f {
     float x;
@@ -25,6 +17,33 @@ struct Person {
     int age;
 };
 
+// 通用的结构体转换为 JSON 的模板函数
+template <typename T>
+json structToJson(const T& obj) {
+    json jsonObj;
+    jsonObj["Unknown Type"] = "Cannot Convert to JSON";
+    return jsonObj;
+}
+
+// 模板特化版本，用于将 Vector3f 结构体转换为 JSON
+template <>
+json structToJson(const Vector3f& obj) {
+    json jsonObj;
+    jsonObj["x"] = obj.x;
+    jsonObj["y"] = obj.y;
+    jsonObj["z"] = obj.z;
+    return jsonObj;
+}
+
+// 模板特化版本，用于将 Person 结构体转换为 JSON
+template <>
+json structToJson(const Person& obj) {
+    json jsonObj;
+    jsonObj["name"] = obj.name;
+    jsonObj["age"] = obj.age;
+    return jsonObj;
+}
+
 int main() {
     Vector3f myVector = {1.0f, 2.0f, 3.0f};
     Person person = {"John", 30};
@@ -34,6 +53,12 @@ int main() {
 
     std::cout << "Vector JSON: " << vectorJson.dump(4) << std::endl;
     std::cout << "Person JSON: " << personJson.dump(4) << std::endl;
+
+    json myJson;
+    myJson["vectorJson"] = vectorJson;
+    myJson["personJson"] = personJson;
+    std::string jsonString = myJson.dump();
+    std::cout << jsonString << std::endl;
 
     return 0;
 }
---
+++ ProjectStudy/redisRemote/Timer/TimerFunction.cpp
@@ -0,0 +1,83 @@
+/*
+可以运行在linux中直接，每1秒刷新一次数据库，添加的是多个结构体结合的Json格式，即最终格式
+*/
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include "RedisConnect.h"
+#include "nlohmann/json.hpp"
+
+using json = nlohmann::json;
+using namespace std;
+
+struct Vector3f {
+    float x;
+    float y;
+    float z;
+};
+
+struct Person {
+    std::string name;
+    int age;
+};
+
+// 重载版本1：将 Vector3f 转换为 JSON
+json structToJson(const Vector3f& obj) {
+    json jsonObj;
+    jsonObj["x"] = obj.x;
+    jsonObj["y"] = obj.y;
+    jsonObj["z"] = obj.z;
+    return jsonObj;
+}
+
+// 重载版本2：将 Person 结构体转换为 JSON
+json structToJson(const Person& obj) {
+    json jsonObj;
+    jsonObj["name"] = obj.name;
+    jsonObj["age"] = obj.age;
+    return jsonObj;
+}
+
+// 要执行的函数
+void myFunction() {
+    string val;
+    int UAVIP = 001;
+    int DATA = 1;
+    RedisConnect::Setup("172.21.2.1", 6379, "Ustc1958@2023");
+    shared_ptr<RedisConnect> redis = RedisConnect::Instance();
+    redis->select(11);
+
+    Vector3f myVector = {1.0f, 2.0f, 3.0f};
+    Person person = {"John", 30};
+    json vectorJson = structToJson(myVector);
+    json personJson = structToJson(person);
+
+    json myJson;
+    myJson["myVector"] = vectorJson;
+    myJson["personJson"] = personJson;
+    cout << myJson << endl;
+    string jsonString = myJson.dump();
+    while (true) {
+        // 在这里执行你的任务
+        if(UAVIP == 005){
+            UAVIP = 001;
+        }
+        redis->set("ma_test:" + to_string(UAVIP) + "$", jsonString);
+        UAVIP++;
+        DATA++;
+        cout << "插入成功一次" << endl;
+        this_thread::sleep_for(chrono::seconds(1));
+    }
+}
+
+int main() {
+    // 创建一个单独的线程，用于执行myFunction
+    thread myThread(myFunction);
+
+    // 主线程可以执行其他任务
+
+    // 等待myThread完成
+    myThread.join();
+
+    return 0;
+}
